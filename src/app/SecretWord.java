52f72b4552bcd8f5fd32b8e58901b24e8dfc8666
Filip Rydberg <fr222cy@student.lnu.se>
2016-11-10 14:23:21 +0100

    REFACTOR

--- src/app/SecretWord.java
+++ src/app/SecretWord.java
@@ -8,12 +8,12 @@ package app;
  *
  */
 public class SecretWord {
-
-	public SecretWord(int num) throws Exception {
-		if(num < 3){
+	private int _amountOfLetters;
+	public SecretWord(int amountOfLetters) throws Exception {
+		if(amountOfLetters < 3){
 			throw new Exception("Have to be more than 3");
 		}
-		
+		_amountOfLetters = amountOfLetters;
 	}
 
 }
